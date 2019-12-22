df098875a91f3a89dba73ea7e693cc74bcff5cb1
Athreya <23Athreyad@students.harker.org>
2019-12-22 15:56:20 -0800

    parsing the kinase names off the website and putting them into csv format

---
+++ kinasenetrequests.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+response = requests.get("https://www.uniprot.org/docs/pkinfam")
+soup = bs(response.text,"lxml")
+links = soup.findAll('a')
+links = links[39:1066]
+kinase_list = []
+label = ["kinase"]
+for i in links:
+    kinase_list.append(i.text)
+df = pd.DataFrame(kinase_list,columns = label)
+print(df)
