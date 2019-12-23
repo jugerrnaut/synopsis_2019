7202d52b14be572a2446c4694ae31f43ad16b55e
Athreya <23Athreyad@students.harker.org>
2019-12-22 16:50:31 -0800

    got kinasenet url

--- kinasenetrequests.py
+++ kinasenetrequests.py
@@ -1,6 +1,7 @@
 import requests
 import selenium
 from selenium import webdriver
+import time
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 response = requests.get("https://www.uniprot.org/docs/pkinfam")
@@ -15,3 +16,10 @@ df = pd.DataFrame(kinase_list,columns = label)
 
 path_to_driver = "/Users/athreya/Downloads/chromedriver"
 driver = webdriver.Chrome(path_to_driver)
+
+url = "http://www.kinasenet.ca/showProtein"
+query = "AurA"
+driver.get(url)
+sbox = driver.find_element_by_name("query")
+sbox.send_keys(query)
+submit_button = driver.find_element_by_id("search").click()
