fd501743dc58ad0e184d9a66c4fbe20ae40d0eea
Athreya <23Athreyad@students.harker.org>
2019-12-22 16:41:22 -0800

    added in selenium for automation

--- kinasenetrequests.py
+++ kinasenetrequests.py
@@ -1,4 +1,6 @@
 import requests
+import selenium
+from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 response = requests.get("https://www.uniprot.org/docs/pkinfam")
@@ -10,4 +12,6 @@ label = ["kinase"]
 for i in links:
     kinase_list.append(i.text)
 df = pd.DataFrame(kinase_list,columns = label)
-print(df)
+
+path_to_driver = "/Users/athreya/Downloads/chromedriver"
+driver = webdriver.Chrome(path_to_driver)
