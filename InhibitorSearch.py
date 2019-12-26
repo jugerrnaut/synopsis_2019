2c2273861dee1a50499f2dc5b443bcedf064d4eb
Athreya <23Athreyad@students.harker.org>
2019-12-26 11:16:54 -0800

    adjusting code for efficiency

--- InhibitorSearch.py
+++ InhibitorSearch.py
@@ -3,18 +3,25 @@ import pandas as pd
 import csv
 from bs4 import BeautifulSoup as bs
 import requests
-
-def getInhibitors(kinase):
-    path_to_driver = "/Users/23gordonc/Downloads/chromedriver"
+import pandas as pd
+import joblib
+import numpy as np
+df = pd.read_csv("kinases.csv")
+def getInhibitors(kinasedf,column,path_to_driver = "/Users/athreya/Downloads/chromedriver",url = "http://www.kinasenet.ca/showProtein"):
     driver = webdriver.Chrome(path_to_driver)
-    url = "http://www.kinasenet.ca/showProtein"
-    query = kinase
-    driver.get(url)
-    sbox = driver.find_element_by_name("query")
-    sbox.send_keys(query)
-    submit_button = driver.find_element_by_id("search").click()
-    results = driver.find_elements_by_class_name("name")
-    inhibitorList = []
-    for i in results:
-        inhibitorList.append(i.text)
-        return inhibitorList
+    try:
+        for i in kinasedf["{}".format(column)]: 
+            driver.get(url)
+            sbox = driver.find_element_by_name("query")
+            sbox.send_keys(i)
+            submit_button = driver.find_element_by_id("search").click()
+            results = driver.find_elements_by_class_name("name")
+            inhibitorList = []
+            for i in results:
+                inhibitorList.append([i,i.text])
+        print(inhibitorList)
+        joblib.dump(np.array(inhibitorList),"myinhibitors.npy")
+    except:
+        print(inhibitorList)
+        joblib.dump(np.array(inhibitorList),"myinhibitors.npy")
+getInhibitors(df, "Short Name")
