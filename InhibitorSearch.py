4c6d4cc17265e249a90c6da0a503560ce8a82e36
Athreya <23Athreyad@students.harker.org>
2019-12-26 21:32:42 -0800

    converted searching to a function, returned a file

--- InhibitorSearch.py
+++ InhibitorSearch.py
@@ -7,21 +7,24 @@ import pandas as pd
 import joblib
 import numpy as np
 df = pd.read_csv("kinases.csv")
-def getInhibitors(kinasedf,column,path_to_driver = "/Users/athreya/Downloads/chromedriver",url = "http://www.kinasenet.ca/showProtein"):
-    driver = webdriver.Chrome(path_to_driver)
+path_to_driver = "/Users/athreya/Downloads/chromedriver"
+driver = webdriver.Chrome(path_to_driver)
+url = "http://www.kinasenet.ca/showProtein"
+inhibitorList = []
+def getinhibitors():
     try:
-        for i in kinasedf["{}".format(column)]: 
+        for i in df["{}".format("Short Name")]: 
             driver.get(url)
             sbox = driver.find_element_by_name("query")
             sbox.send_keys(i)
             submit_button = driver.find_element_by_id("search").click()
             results = driver.find_elements_by_class_name("name")
-            inhibitorList = []
-            for i in results:
-                inhibitorList.append([i,i.text])
+            for n in results:
+                inhibitorList.append([i,n.text])
         print(inhibitorList)
-        joblib.dump(np.array(inhibitorList),"myinhibitors.npy")
+        joblib.dump(inhibitorList,"myinhibitors.npy")
     except:
         print(inhibitorList)
-        joblib.dump(np.array(inhibitorList),"myinhibitors.npy")
-getInhibitors(df, "Short Name")
+        joblib.dump(inhibitorList,"myinhibitors.npy")
+    driver.quit()
+#getinhibitors()
